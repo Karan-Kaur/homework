9ea0eefd22fe6f8835b4a2ce5b0b6c09aa3c0edd
Karan-Kaur <75669830+Karan-Kaur@users.noreply.github.com>
2021-07-06 02:20:19 +0100

    Add files via upload

---
+++ Simple_ATM_functions.py
@@ -0,0 +1,30 @@
+#smaller functions to test
+
+#checks for a valid pin that is 4 digits
+def pin(num):
+    valid_pin = range(1000, 9999)
+    if num in valid_pin:
+        return True
+    else:
+        return False
+
+print(pin(209))
+
+#checks given pin is an integer
+def check_int(num1):
+    if num1 is not int:
+        raise TypeError('Please enter integers')
+
+check_int('abc')
+
+#checks withdrawal amount is positive
+def check_pos(num2):
+    if num2 < 0:
+        raise ValueError('Please enter a positive number')
+
+check_pos(-19)
+
+#checks withdrawal calculation
+def check_withdraw(num3):
+    return 100 - num3
+print(check_withdraw(30))
